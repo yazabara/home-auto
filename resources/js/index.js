8bde105ea043a5715b24abd43c8c6ebc2070317c
yazabara <yazabara@gmail.com>
2015-12-15 10:47:41 +0300

    requirejs was added

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,30 @@
-$('.animate').appear(function() {
-  $(this ).addClass('animate_visible');
+require.config({
+    paths: {
+        jquery: '../../bower_components/jquery/dist/jquery.min',
+        jqueryAppear: '../../bower_components/jquery.appear/jquery.appear',
+        bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap.min',
+        less: '../../bower_components/less/dist/less.min'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        },
+        less: {
+            deps: ['jquery']
+        },
+        jqueryAppear: {
+            deps: ['jquery']
+        },
+        jquery: {
+            exports: '$'
+        }
+    }
+});
+
+define(['jquery', 'jqueryAppear', 'bootstrap', 'less'], function ($, jqueryAppear, bootstrap, less) {
+
+    $('.animate').appear(function () {
+        $(this).addClass('animate_visible');
+    });
+
 });
